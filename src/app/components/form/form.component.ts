e86a87591446dd19abb417b05b33b9be7ec3d079
AlexKuskov <a.kuskov98@gmail.com>
2020-02-08 19:54:21 +0200

    add few changes

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { UserForm } from '../../models/user-form';
 
 @Component({
   selector: 'app-form',
@@ -9,9 +10,16 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   form: FormGroup;
+  formResults: UserForm;
 
-  get additionalEmails() {
-    return this.form.get('additionalEmails') as FormArray;
+  countries = [
+    'Ukraine',
+    'Russia',
+    'USA'
+  ];
+
+  get emailAddresses() {
+    return this.form.get('emailAddresses') as FormArray;
   }
 
   ngOnInit() {
@@ -20,27 +28,32 @@ export class FormComponent implements OnInit {
       lastName: new FormControl(),
       areaCode: new FormControl(),
       phoneNumber: new FormControl(),
+      emailAddresses: new FormArray([
+        // new FormControl(null, Validators.email)
+      ]),
       company: new FormControl(),
       streetAddress: new FormControl(),
       streetAddress2: new FormControl(),
       city: new FormControl(),
       stateProvince: new FormControl(),
       postalZipCode: new FormControl(),
-      email: new FormControl(null, Validators.email),
-      additionalEmails: new FormArray([])
+      // email: new FormControl(null, Validators.email),
+      // additionalEmails: new FormArray([]),
+      country: new FormControl(this.countries[0])
     });
   }
 
   addEmail() {
-    this.additionalEmails.push(new FormControl());
+    this.emailAddresses.push(new FormControl(null, Validators.email));
   }
 
   deleteEmail(index: number) {
-    this.additionalEmails.removeAt(index);
+    this.emailAddresses.removeAt(index);
   }
 
-  onSubmit(userForm: any) {
+  onSubmit(userForm: UserForm) {
     console.log(userForm);
+    this.formResults = userForm;
     // {{ formResults | json }}
   }
 
--- src/app/models/user-form.ts
+++ src/app/models/user-form.ts
@@ -1,4 +1,14 @@
-class UserForm {
+export class UserForm {
   firstName: string;
-  secondName: string;
+  lastName: string;
+  areaCode: number;
+  phoneNumber: number;
+  emailAddresses: string[];
+  company: string;
+  streetAddress: string;
+  streetAddress2: string;
+  city: string;
+  stateProvince: string;
+  postalZipCode: number;
+  country: string;
 }
